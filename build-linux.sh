77e8204ac616ad84198d81e90f4e65fddafd81b7
Clifford Carnmo <clifford@carnmo.com>
2021-01-23 20:34:06 +0100

    Directory structure

--- build-linux.sh
+++
@@ -1,2 +0,0 @@
-bin/linux/acme --msvc --color --format cbm -v3 --outfile hw.prg src/hw.asm
-bin/linux/pucrunch -x$0801 -c64 -g55 -fshort hw.prg hw.prg
---
+++ helloworld/build-linux.sh
@@ -0,0 +1,2 @@
+../bin/linux/acme --msvc --color --format cbm -v3 --outfile hw.prg hw.asm
+../bin/linux/pucrunch -x$0801 -c64 -g55 -fshort hw.prg hw.prg
